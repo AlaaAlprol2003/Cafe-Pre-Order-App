6a5c6eef09315caf164128097684b82a28c698a7
omar_tamer <otamer590@gmail.com>
2026-04-15 14:12:33 +0200

    Optimize UI components in product category and product screens; improve layout and styling

--- lib/core/widgets/product_category.dart
+++ lib/core/widgets/product_category.dart
@@ -7,7 +7,7 @@ class ProductCategory extends StatefulWidget {
   const ProductCategory({super.key, required this.product});
 
   final Products product;
-  
+
   @override
   State<ProductCategory> createState() => _ProductCategoryState();
 }
@@ -21,26 +21,25 @@ class _ProductCategoryState extends State<ProductCategory> {
         Container(
           height: 150.h,
           width: double.infinity,
-
           decoration: BoxDecoration(
-            color: ColorsManager.darkChocolate,
+           
+            color: const Color.fromARGB(255, 57, 35, 7),
             borderRadius: BorderRadius.only(topRight: Radius.circular(16.r)),
           ),
         ),
         Positioned(
           bottom: -16.h,
           left: -10.w,
-
           child: Container(
             height: 185.h,
             width: 160.w,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(16.r),
-              color: ColorsManager.darkBrown
+              color: const Color.fromARGB(255, 132, 97, 55),
             ),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(16.r),
-              child: Image.asset(widget.product.image, fit: BoxFit.cover),
+              child: Image.asset(widget.product.image, fit: BoxFit.contain),
             ),
           ),
         ),
@@ -51,19 +50,25 @@ class _ProductCategoryState extends State<ProductCategory> {
             children: [
               Text(
                 widget.product.name,
-                style: Theme.of(context).textTheme.displayMedium!.copyWith(color: ColorsManager.creamyWhite),
+                style: Theme.of(context)
+                    .textTheme
+                    .displayMedium!
+                    .copyWith(color: ColorsManager.creamyWhite),
               ),
               SizedBox(height: 10.h),
               Text(
                 "Pre Time: ${widget.product.pretime}",
                 style: Theme.of(context).textTheme.displayMedium!.copyWith(
-                  color: ColorsManager.creamyWhite,
-                ),
+                      color: ColorsManager.creamyWhite,
+                    ),
               ),
               SizedBox(height: 10.h),
               Text(
                 "EGP ${widget.product.price}",
-                style: Theme.of(context).textTheme.displayMedium!.copyWith(color: ColorsManager.creamyWhite),
+                style: Theme.of(context)
+                    .textTheme
+                    .displayMedium!
+                    .copyWith(color: ColorsManager.creamyWhite),
               ),
               SizedBox(height: 10.h),
             ],
@@ -77,23 +82,32 @@ class _ProductCategoryState extends State<ProductCategory> {
               Text(
                 "${widget.product.rate}",
                 style: Theme.of(context).textTheme.displayMedium!.copyWith(
-                  color: ColorsManager.creamyWhite,
-                  fontSize: 15.sp
-                ),
+                    color: ColorsManager.creamyWhite, fontSize: 15.sp),
               ),
               SizedBox(width: 5.w),
-              Icon(Icons.star, color: Colors.amber,size: 15.h,),
+              Icon(
+                Icons.star,
+                color: Colors.amber,
+                size: 15.h,
+              ),
             ],
           ),
         ),
         Positioned(
           bottom: -10.h,
           right: -10.w,
-          child: CircleAvatar(
-            backgroundColor: ColorsManager.burntOrange,
-
-            radius: 20,
-            child: Icon(Icons.add, color: ColorsManager.darkNavyBlue, size: 20),
+          child: Container(
+            height: 30.h,
+            width: 40.w,
+            decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 132, 97, 55),
+                borderRadius: BorderRadius.circular(8.r)),
+            child: Icon(
+              Icons.add_outlined,
+              color: ColorsManager.creamyWhite,
+              size: 25.h,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
       ],
--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -67,7 +67,6 @@ class _LoginScreenState extends State<LoginScreen> {
             }
           },
         ),
-
         BlocListener<AuthCubit, AuthState>(
           listenWhen: (prev, curr) =>
               curr is GetUserFromFirestoreSuccess ||
@@ -80,7 +79,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 context: context,
                 message: " Welcome back! We're glad to see you 👋",
               );
-              ///Navigator.pushReplacementNamed(context, AppRoutes.mainLayout);
+              Navigator.pushReplacementNamed(context, AppRoutes.mainLayout);
             } else if (state is GetUserFromFirestoreFailure) {
               UiUtils.showMessage(
                 context: context,
@@ -102,8 +101,9 @@ class _LoginScreenState extends State<LoginScreen> {
               padding: REdgeInsets.only(top: 130.0),
               child: Center(
                 child: ConstrainedBox(
-                  constraints: BoxConstraints(maxWidth: 400.w,),
-                  
+                  constraints: BoxConstraints(
+                    maxWidth: 400.w,
+                  ),
                   child: Column(
                     children: [
                       Image.asset(
@@ -164,15 +164,15 @@ class _LoginScreenState extends State<LoginScreen> {
                               SizedBox(height: 24.h),
                               CustomElevatedButton(
                                 onPressed: () async {
-                                  Navigator.pushReplacementNamed(context, AppRoutes.mainLayout);
-                                  // if (_formKey.currentState?.validate() == false)
-                                  //   return;
-                                  // await cubit.login(
-                                  //   request: LoginRequest(
-                                  //     email: _emailController.text,
-                                  //     password: _passwordController.text,
-                                  //   ),
-                                  // );
+                                  //Navigator.pushReplacementNamed(context, AppRoutes.mainLayout);
+                                  if (_formKey.currentState?.validate() ==
+                                      false) return;
+                                  await cubit.login(
+                                    request: LoginRequest(
+                                      email: _emailController.text,
+                                      password: _passwordController.text,
+                                    ),
+                                  );
                                 },
                                 text: "Login",
                               ),
@@ -197,7 +197,7 @@ class _LoginScreenState extends State<LoginScreen> {
                                   ),
                                 ],
                               ),
-                  
+
                               // Padding(
                               //   padding: REdgeInsets.only(top: 60.0),
                               //   child: CustomAnimatedToggle(),
